58869cdb320c997e4dba4214799b4d6ae5cf07ba
Buddhadeb Banik <buddhadebbanik96@gmail.com>
2026-08-24 05:24:49 +0530

    Add function to check if a number is a palindrome

---
+++ Emb-DSA/Leetcode/#9-Palindrome.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+
+bool isPalindrome(int x) {
+if (x < 0)
+        return false;
+
+    long long reverse =0;
+    int temp = x;
+    while (temp != 0) {
+       reverse = (reverse * 10) + (temp % 10);
+        temp = temp / 10;}
+
+        return(reverse == x);
+}
